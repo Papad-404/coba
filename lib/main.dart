78914576ec4700eb6e45fa47afcbd297c41385d4
Papad-404 <59909840+Papad-404@users.noreply.github.com>
2020-11-24 20:55:32 +0700

    ngepull

---
+++ lib/main.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  gotoSecondact(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Kategory()),
+    );
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(),
+          body: new Stack(children: <Widget>[
+            new Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/wllpaper2.jpg'),
+                      fit: BoxFit.cover)),
+            ),
+            new Column(children: <Widget>[
+              Container(
+                child: RaisedButton(
+                  child: Text('Start'),
+                  color: Colors.black54,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    gotoSecondact(context);
+                  },
+                ),
+              ),
+              Container(
+                child: RaisedButton(
+                  child: Text('Quit'),
+                  color: Colors.black54,
+                  textColor: Colors.white,
+                  onPressed: () {},
+                ),
+              ),
+            ])
+          ])),
+    );
+  }
+}
+
+class Kategory extends StatelessWidget {
+  gotokum1(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Kumpulan1()),
+    );
+  }
+
+  gotokum2(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Kumpulan2()),
+    );
+  }
+
+  gotokum3(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Kumpulan3()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Categories"),
+        ),
+        body: new Stack(children: <Widget>[
+          new Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/wllpaper2.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+          new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: RaisedButton(
+                    child: Text('Kategori 1'),
+                    color: Colors.black54,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      gotokum1(context);
+                    },
+                  ),
+                ),
+                Container(
+                  child: RaisedButton(
+                    child: Text('Kategori 2'),
+                    color: Colors.black54,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      gotokum2(context);
+                    },
+                  ),
+                ),
+                Container(
+                  child: RaisedButton(
+                    child: Text('Kategori 3'),
+                    color: Colors.black54,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      gotokum3(context);
+                    },
+                  ),
+                ),
+              ])
+        ]));
+  }
+}
+
+class Kumpulan1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Stories"),
+        ),
+        body: new Stack(children: <Widget>[
+          new Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/wllpaper2.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+          new Column(children: <Widget>[
+            Container(
+              child: RaisedButton(
+                child: Text('Cerita 1'),
+                color: Colors.black54,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              child: RaisedButton(
+                child: Text('Cerita 2'),
+                color: Colors.black54,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+          ])
+        ]));
+  }
+}
+
+class Kumpulan2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Stories"),
+        ),
+        body: Center(
+            child: Column(children: <Widget>[
+          Container(
+            child: RaisedButton(
+              child: Text('Cerita 1'),
+              color: Colors.black54,
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+          Container(
+            child: RaisedButton(
+              child: Text('Cerita 2'),
+              color: Colors.black54,
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+        ])));
+  }
+}
+
+class Kumpulan3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Stories"),
+        ),
+        body: Center(
+            child: Column(children: <Widget>[
+          Container(
+            child: RaisedButton(
+              child: Text('Cerita 1'),
+              color: Colors.black54,
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+          Container(
+            child: RaisedButton(
+              child: Text('Cerita 2'),
+              color: Colors.black54,
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ),
+        ])));
+  }
+}
